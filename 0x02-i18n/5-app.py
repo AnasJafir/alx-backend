2b3fcf240dfc91ae76b806f1bd424062e4ec6712
AnasJafir <dr.anas.jafir@gmail.com>
2024-07-06 19:49:45 +0000

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-""" Basic Flask app. """
+'''Task 4: Force locale with URL parameter
+'''
+
+from typing import Dict, Union
 from flask import Flask, render_template, request, g
-from flask_babel import Babel, gettext
+from flask_babel import Babel
 
 
 class Config:
-    """ Config class for the Babel object. """
+    '''Config class'''
+
+    DEBUG = True
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -13,6 +18,7 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 users = {
@@ -24,46 +30,44 @@ users = {
 
 
 def get_user() -> Union[Dict, None]:
+    """Retrieves a user based on a user id.
     """
-    Returns a user dictionary
-    None if the ID cannot be found
-    or if value is missing
-    """
-    user_id = request.args.get('login_as')
-    if user_id:
-        return users.get(int(user_id))
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
     return None
 
 
 @app.before_request
 def before_request() -> None:
-    """ Makes sure the user is logged in before each request. """
+    """Performs some routines before each request's resolution.
+    """
+
     g.user = get_user()
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """ Determines the best language to use for the user. """
+    """Retrieves the locale for a web page.
+
+    Returns:
+        str: best match
+    """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/')
 def index() -> str:
-    """
-    Renders the '1-index.html' template and returns it as a response.
-
-    This function is a Flask route that is mapped to the root URL ("/").
-    It is responsible for displaying the home page of the application.
+    '''default route
 
     Returns:
-        A rendered HTML template.
-    """
-    return render_template('5-index.html')
+        html: homepage
+    '''
+    return render_template("5-index.html")
 
 
 if __name__ == "__main__":
-    """ Runs the Flask app. """
     app.run()
